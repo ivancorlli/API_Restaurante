5b4f0ee6eb75bc2b76baf8fb8f98150fc934232f
Ivan Corlli <corlliivan@gmail.com>
2021-10-31 17:14:08 -0300

    Configuracion Inicial

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require( 'express');
+
+
+const app = express();
+
+
+
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// Rutas
+app.get('/',(req,res)=>{
+    return res.send('Server WORKS')
+})
+
+module.exports =  app
---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const  mongoose  = require( "mongoose");
+const app = require( './app');
+
+
+
+(async function connect(){
+    try{
+        await mongoose.connect(process.env.DB_URI,{});
+        console.log('\x1b[36m La conexión a la base de Datos se realizo correctamente \x1b[37m');
+        app.listen(process.env.PORT, () => {
+            console.log('\x1b[33m Server started on port ' + PORT + '\x1b[37m');
+        });
+    }catch (err) {
+        console.log('\x1b[31m Error al conectar a la base de datos \x1b[37m', err);
+    }
+})();
