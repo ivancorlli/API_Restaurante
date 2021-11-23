8aef2507662135f109d18d3dbda8b99b606e21e8
Ivan Corlli <corlliivan@gmail.com>
2021-11-23 16:07:18 -0300

    23

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const api = express.Router();
 
+// Middlewares
+const auth = require('../middlewares/auth')
 
 // controllers
 const menuControllers = require('../controllers/menuControllers');
 
 
-api.post('/menu', menuControllers.createMenu);
-api.get('/menu', menuControllers.getMenus)
+api.post('/menu',auth, menuControllers.createMenu);
+api.get('/menu',auth, menuControllers.getMenus)
 
 module.exports = api
--- schema/menuSchema.js
+++ schema/menuSchema.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 const menuSchema = new Schema ({
     nombre:{type:String, required:true},
+    categoria:{type:String, required:true},
     precio:{type:Number, required:true},
     descripcion:{type:String, required:true},
     imagen:{type:String},
